68055a7673569c0e128b8729457a271e70b3a7a5
Manmeet <manmeetsgujral@gmail.com>
2016-07-28 14:53:49 -0400

    Added some login credentials

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import FacebookLogin from 'react-facebook-login';
 
 // example class based component (smart component)
 class App extends Component {
@@ -8,13 +9,30 @@ class App extends Component {
     this.state = {
 
     };
+    this.onFacebookLogin = this.onFacebookLogin.bind(this);
   }
 
-
+  onFacebookLogin(response) {
+    if (response.status !== 'not_authorized' && response.status !== 'unknown') {
+      document.getElementById('facebook').innerHTML = `Welcome, ${response.name}! `;
+      document.getElementById('facebook').innerHTML += `<img src="${response.picture.data.url}"/>`;
+      document.getElementById('facebook').style = 'display: block';
+    } else {
+      document.getElementById('facebook').style = 'display: block';
+    }
+  }
   render() {
     // Add in FacebookLogin, GoogleLogin and FirebaseApp
     return (
       <div id="login">
+        <div id="facebook">
+          <FacebookLogin
+            appId="yourappid"
+            autoLoad
+            fields="name,email,picture.type(large)"
+            callback={this.onFacebookLogin}
+          />
+        </div>
       </div>
     );
   }
